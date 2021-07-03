305f10be6fdd27e936cd0f803cf8dbc8d593379b
kaoru kubota <kaoru64kubo@gmail.com>
2021-07-03 13:12:32 +0900

    master

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # get 'tweets/new'
+  # get 'tweets/index'
+  # get 'tweets/show'
+  # get 'tweets/create'
+  get 'users/index'
+  # get 'users/show'
+  devise_for :users
+  resources :tweets
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
 end
